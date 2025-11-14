4ed49f83174fe5ec6bd453a7f7f62f928e453c4a
Nyzurbern <Hrithik_tec@outlook.sg>
2025-11-14 15:46:10 +0800

    Initial Commit

---
+++ SomeGoalsApp/SomeGoalsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeGoalsAppApp.swift
+//  SomeGoalsApp
+//
+//  Created by T Krobot on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SomeGoalsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
