adc8873411b668cb83d0ced21c473a21797c8067
Khoo Zi Qi <qiqicool7788@gmail.com>
2025-11-14 16:41:37 +0800

    Initial Commit

---
+++ SomeGoalsApp/GoalDuePopupView.swift
@@ -0,0 +1,41 @@
+//
+//  goalDue.swift
+//  SomeGoalsApp
+//
+//  Created by T Krobot on 14/11/25.
+//
+
+import SwiftUI
+
+struct GoalDuePopupView: View {
+
+    var body: some View {
+        VStack {
+            
+            Text("GOAL")
+            Text("IS DUE!!!")
+         
+        }
+        .bold()
+        .foregroundStyle(.red)
+        .font(.title)
+        
+        Text("How's your progress?")
+            .foregroundStyle(.red)
+        
+        VStack {
+            Button("I didn't manage to achieve it...", action: {
+            })
+                .tint(.red)
+            Button("I need.... MORE TIME!!!🕰️", action: {})
+                .tint(.yellow)
+            Button("I DID IT😍💪 #worlddomination", action: {})
+                .tint(.green)
+        }
+        .buttonStyle(.borderedProminent)
+    }
+}
+
+#Preview {
+    GoalDuePopupView()
+}
