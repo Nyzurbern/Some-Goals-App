35d3b6232abd62c57d1aa8c964733b0725cda0f9
Khoo Zi Qi <qiqicool7788@gmail.com>
2025-11-15 11:47:44 +0800

    asrfdgfh

--- SomeGoalsApp/FailOrSuccessView.swift
+++ SomeGoalsApp/FailOrSuccessView.swift
@@ -7,6 +7,8 @@
 import SwiftUI
 
 struct FailOrSuccessView: View {
+    @EnvironmentObject var userData: UserData
+    @State private var FinalReflectionDone = false
     @State private var reflection = ""
     var body: some View {
         VStack {
@@ -20,12 +22,24 @@ struct FailOrSuccessView: View {
                 .textFieldStyle(.roundedBorder)
         }
         .padding(40)
-    
+        
         Text("See how far your character has come!")
             .frame(width: 350)
             .bold()
             .font(.title3)
-
+        
+        Button {
+            FinalReflectionDone = true
+        } label: {
+            Text("Next")
+                .padding(.vertical, 10)
+                .padding(.horizontal, 14)
+                .background(RoundedRectangle(cornerRadius: 10) .fill(Color.blue))
+                .foregroundStyle(.white)
+        }
+        .navigationTitle("")
+        .sheet(isPresented: $FinalReflectionDone) {
+            HomeView().environmentObject(userData)}
     }
 }
 
